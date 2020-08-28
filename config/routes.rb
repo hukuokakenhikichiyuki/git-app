be0e433eef525ff9f52bb458cf17e8645dcec3a9
hukuokakenhikichiyuki <em197872@icloud.com>
2020-08-28 14:39:49 +0900

    postsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-Risld.applicstion.routes.drow do
-  # For detail on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
